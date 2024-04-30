aca3ae3acb056f007376ec190e7ce8f12ddff26f
Giovanni Rodriguez <gio.rodriguez@mongodb.com>
2024-04-30 10:43:06 -0600

    Removed secrets from file

--- rag_mongodb.py
+++ rag_mongodb.py
@@ -1,5 +1,5 @@
 from datasets import load_dataset
-from dotenv import load_dotenv
+from dotenv import dotenv_values
 import os
 import pandas as pd
 import openai
@@ -18,12 +18,13 @@ dataset_df = dataset_df.dropna(subset=['plot'])
 print("Number of missing values in each column after removal:")
 print(dataset_df.isnull().sum())
 
+dataset_df_embedded_values = dataset_df.copy()
 # Remove the plot_embedding from each data point in the dataset as we are going to create new embeddings with the new OpenAI embedding Model "text-embedding-3-small"
 dataset_df = dataset_df.drop(columns=['plot_embedding'])
 print(dataset_df.head()['plot'])
 
 #Importing OpenAI apikey
-loaded_secrets = load_dotenv(".env")
+loaded_secrets = dotenv_values(".env")
 openai.api_key = loaded_secrets["API_KEY"]
 
 #Selecting the OpenAI embedding model
@@ -39,11 +40,13 @@ def get_embedding(text):
 
     try:
         # Call OpenAI API to get the embedding
-        embedding = openai.embeddings.create(input=text, model=EMBEDDING_MODEL).data[0].embedding
-        return embedding
+        # embedding = openai.embeddings.create(input=text, model=EMBEDDING_MODEL).data[0].embedding
+        # return embedding
+        print("Simulating the request to OPENAI")
     except Exception as e:
         print(f"Error in get_embedding: {e}")
         return None
     
-dataset_df["plot_embedding_optimised"] = dataset_df['plot'].apply(get_embedding)
-print(dataset_df.head()['plot_embedding_optimised'])    
+#dataset_df["plot_embedding_optimised"] = dataset_df['plot'].apply(get_embedding)
+dataset_df["plot_embedding_optimised"] = dataset_df_embedded_values['plot_embedding']
+print(dataset_df.head()['plot_embedding_optimised'])
